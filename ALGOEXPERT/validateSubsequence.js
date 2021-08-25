aebc30cbb62f45aedc6bbce8a84bea780c7468c6
Deepak Garg <erdkgarg08@gmail.com>
2021-08-25 14:05:24 +0200

    validate subsequence

---
+++ ALGOEXPERT/validateSubsequence.js
@@ -0,0 +1,24 @@
+// https://github.com/deepakgarg08/algoexpert-data-structures-algorithms/blob/master/Questions/validate-subsequence.PNG
+
+const array = [5, 1, 22, 25, 6, -1, 8, 10];
+const sequence = [1, 6, -1, 10];
+let j = 0;
+let counter = 0;
+
+function subSequence(array, sequence) {
+  for (let i = 0; i < sequence.length; i++) {
+    while (j < array.length) {
+      if (sequence[i] === array[j]) {
+        counter++;
+        break
+      } else {
+        j++;
+      }
+    }
+
+  }
+
+  return counter === sequence.length ? true : false
+
+}
+console.log("subsequence:: ", subSequence(array, sequence));
