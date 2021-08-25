8bdaacf015bd94754714ef11b9e6cdec7c168832
Deepak Garg <erdkgarg08@gmail.com>
2021-08-25 12:54:51 +0200

    remove all repeated elements in sorted linked list

--- ALGOEXPERT/removeDuplicateFromLInkedList.js
+++ ALGOEXPERT/removeDuplicateFromLInkedList.js
@@ -1,6 +1,6 @@
 //https://github.com/deepakgarg08/algoexpert-data-structures-algorithms/blob/master/Questions/remove-duplicates-from-linked-list.PNG
 // 1 2 3 4 4 5
-const LinkedList = require('../FUNDAMENTALS/Linkedlist/lllogic');
+const LinkedList = require("../FUNDAMENTALS/Linkedlist/lllogic");
 const util = require("util");
 
 let ll = new LinkedList();
@@ -10,43 +10,29 @@ ll.insert(3);
 ll.insert(4);
 ll.insert(4);
 ll.insert(5);
+ll.insert(5);
+ll.insert(5);
 console.log(util.inspect(ll, { showHidden: false, depth: null }));
-
+console.log("...........");
 
 const removeDuplicateFromLinkedList = (ll) => {
-    // console.log('ll', ll.value)
 
-    let valuesStored = {}
-    let ourll = ll
-    ll =ll.head
-
-    while(ll){
-        valuesStored[ll.value] = ll.value
-        // console.log("check ll.value", ll.next.value);
-        if(ll.next.value in valuesStored){
-            ll.next = ll.next.next
-            ourll.size--
-            return ourll
-        }
-        ll = ll.next
+  let ourll = ll;
+  ll = ll.head;
+  while (ll) {
+    if (!ll.next) {
+      return ourll;
     }
 
-    
-}
+    if (ll.value === ll.next.value) {
+      ll.next = ll.next.next;
+      ourll.size--;
+    } else {
+      ll = ll.next;
+    }
+  }
+  return ourll;
+};
 
-let fr = removeDuplicateFromLinkedList(ll)
-// console.log('removeDuplicateFromLinkedList', removeDuplicateFromLinkedList(ll.head))
-console.log(util.inspect(fr , { showHidden: false, depth: null }));
-
-
-
-
-
-
-
-
-
-
-
-
-
+let fr = removeDuplicateFromLinkedList(ll);
+console.log(util.inspect(fr, { showHidden: false, depth: null }));
