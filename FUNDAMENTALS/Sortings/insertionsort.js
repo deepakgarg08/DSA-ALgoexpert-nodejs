ba03c9c6296f5730f58c5e1f42243fbc187cae19
Deepak Garg <erdkgarg08@gmail.com>
2021-08-26 10:35:17 +0200

    insertion sort updated

--- FUNDAMENTALS/Sortings/insertionsort.js
+++ FUNDAMENTALS/Sortings/insertionsort.js
@@ -4,23 +4,55 @@
 
 var array =[5, 1, 4 ,2 ,8 ]
 var array =[64, 34, 25, 12, 22, 11, 90];
+// 1 5 4 2 8 | 1
+// 1 4 5 2 8
+// 1 
 
-function insertionSort(array){
-    
-    for (let i = 1; i < array.length-1; i++) {
-    
-        for (let j = 0; j < array.length - 1; j++) {
+function insertionSort(arr){
+    console.time("start-m1")
+    n = arr.length
+    let i, key, j; 
+    for (i = 1; i < n; i++)
+    { 
+        key = arr[i]; 
+        j = i - 1; 
+   
+        /* Move elements of arr[0..i-1], that are 
+        greater than key, to one position ahead 
+        of their current position */
+        while (j >= 0 && arr[j] > key)
+        { 
+            arr[j + 1] = arr[j]; 
+            j = j - 1; 
+        } 
+        arr[j + 1] = key; 
 
-            if(array[j ] > array[i]){
-                //swap values
-                [array[j], array[i]] = [ array[i], array[j]]
-            }
+    } 
+    console.timeEnd("start-m1")
+    console.time("start-m2")
+    // method 2 
+    let array = arr.slice()
+    for (let i = 1; i < array.length; i++) {
+    
+        let current, previous;
+        previous = i-1
+        current = array[i]
+
+        while(previous >= 0 && array[previous] > current){
+
+            array[previous+1] = array[previous]
+            array[previous] = current
+            previous--
         }
-    }
-    return array
+        
+    }  
+    console.timeEnd("start-m2")
+    console.log("from 2nd for loop result::",array)
+    return arr
 }
 
 console.log('insertionSort()', insertionSort(array))
+// console.log('insertionSort()', insertionSort(array))
 
 
 
