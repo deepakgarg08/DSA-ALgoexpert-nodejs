0787af194b1a1d26807081f805b9a93ae7009d2d
Deepak Garg <erdkgarg08@gmail.com>
2021-08-27 18:56:15 +0200

    three numbers sum done

--- ALGOEXPERT/threeNumberSum.js
+++ ALGOEXPERT/threeNumberSum.js
@@ -1,8 +1,39 @@
+// O(n*n) O(n) 
+var array = [12, 3, 1, 2, -6, 5, -8, 6];
+var target = 0;
+function threeNumbersSum2(array,target) {
+  array.sort((a, b) => a - b);
+  let triplets = [];
+  for (let i = 0; i < array.length - 2; i++) {
+    let left = i + 1;
+    let right = array.length - 1;
+    while (left < right) {
+      let sum = array[i] + array[left] + array[right];
+      if (sum === target) {
+        triplets.push([array[i], array[left], array[right]]);
+        left++;
+        right--;
+      } else if (sum > target) {
+        right -= 1;
+      } else if (sum < target) {
+        left += 1;
+      }
+    }
+  }
+  return triplets;
+}
+console.log("threeNumbersSum2:: ", threeNumbersSum2(array,target));
+
+
+
+
+// method 2 tried but not working efficiently
+
 var array = [12, 3, 1, 2, -6, 5, -8, 6];
 // var array = [2,3,-5];
-let targetSum = 5;
+let targetSum = 0;
 function threeNumbersSum(array) {
-    // array.sort((a, b) => a - b);
+  // array.sort((a, b) => a - b);
   //   console.log("array", array);
 
   let subArray = [];
@@ -20,29 +51,21 @@ function threeNumbersSum(array) {
         break;
       } else if (m == array[j]) {
         break;
-      }
-      else if (-m == array[i]) {
+      } else if (-m == array[i]) {
         break;
-      }
-      else if (-m == array[j]) {
+      } else if (-m == array[j]) {
         break;
-      }
-
-      else if (m in numberMap) {
+      } else if (m in numberMap) {
         subArray.push(m, array[i], array[j]);
         resultArray.push(subArray);
         subArray = [];
-        break
+        break;
       }
     }
   }
-  let newresult = [];
-  console.log("resultArray", resultArray);
-  for (let i = 0; i < resultArray.length; i++) {
-    resultArray[0][i];
-    newresult.push(resultArray[i])
-  }
-  console.log("newresult", newresult);
+
+
+  return resultArray
 }
 
-console.log("threeNumbersSum:: ", threeNumbersSum(array));
+console.log("threeNumbersSum: getting duplicates: ", threeNumbersSum(array));
