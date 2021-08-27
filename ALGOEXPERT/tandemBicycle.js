2a1824c3dfdf078dd29dad6693983c35c8f9a381
Deepak Garg <erdkgarg08@gmail.com>
2021-08-27 10:43:08 +0200

    tandem bicycle optimised ....

--- ALGOEXPERT/tandemBicycle.js
+++ ALGOEXPERT/tandemBicycle.js
@@ -1,16 +1,15 @@
 //https://github.com/deepakgarg08/algoexpert-data-structures-algorithms/blob/master/Questions/tandem-bicycle.PNG
-//nlogn
-//initially inspite of sum variable, array was used to collect values and using reduce method values were added, which was
-//inefficient
-function tandemBicycle() {
+//nlogn O(n)
+
+let redshirt = [5, 5, 3, 9, 2];
+let blueshirt = [3, 6, 7, 2, 1];
+let fastest = true;
+function tandemBicycle(redshirt, blueshirt, fastest) {
   console.time("time");
-  let redshirt = [5, 5, 3, 9, 2];
-  let blueshirt = [3, 6, 7, 2, 1];
+  //if just in case pair is required for printing
   let pair = [];
-  let fastest = true;
   let res_max_min = 0;
   let sum = 0;
-
   redshirt.sort((a, b) => b - a);
   blueshirt.sort((a, b) => a - b);
 
@@ -25,10 +24,31 @@ function tandemBicycle() {
       res_max_min = pair[i][0] < pair[i][1] ? pair[i][0] : pair[i][1];
 
       sum += res_max_min;
+      sum += res_max_min;
     }
   }
   console.timeEnd("time");
   return sum;
 }
 
-console.log("tandemBicycle:: ", tandemBicycle());
+console.log("tandemBicycle:: ", tandemBicycle(redshirt, blueshirt, fastest));
+
+//method2.. removed array used for pairing i.e O(1) space
+
+function tandemBicycle2(redshirt, blueshirt, fastest) {
+  console.time("time");
+  redshirt.sort((a, b) => b - a);
+  blueshirt.sort((a, b) => a - b);
+  let sum = 0;
+
+  for (let i = 0; i < blueshirt.length; i++) {
+    if (fastest) {
+      sum += Math.max(redshirt[i], blueshirt[i]);
+    } else {
+      sum += Math.min(redshirt[i], blueshirt[i]);
+    }
+  }
+  console.timeEnd("time");
+  return sum;
+}
+console.log("tandemBicycle2:: ", tandemBicycle2(redshirt, blueshirt, false));
