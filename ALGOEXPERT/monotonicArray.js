428153c6893855f18b1199d9c2ac90b56d37aa2b
Deepak Garg <erdkgarg08@gmail.com>
2021-08-28 16:51:14 +0200

    monotonic array optimised method added

--- ALGOEXPERT/monotonicArray.js
+++ ALGOEXPERT/monotonicArray.js
@@ -1,6 +1,6 @@
 // O(n) time and O(1)
 // var array = [-1, 55 , 10, -1100,-1100, -1101,-1102,-9001]
-// var array = [1,1,1,1,1,1,1,1,2,1]
+var array = [1,1,1,1,1,1,1,1,1]
 // var array = [];
 var array = [-1, -5, -10, -1100, -1100, -1101, -1102, -9001];
 
@@ -27,3 +27,30 @@ function monotonicArrayCheck(array) {
 }
 
 console.log("monotonicArrayCheck:: ", monotonicArrayCheck(array));
+
+
+//method2 efficient
+
+function monotonicArrayCheck2(array) {
+    if (array.length < 3) return true;
+    //direction means increasing or decreasing order
+    // -1 , 1 means decreasing and increasing resp
+    let direction = array[1] - array[0] <= 0 ? -1 : 1
+    console.log('direction', direction)
+      
+    for (let i = 0; i < array.length-1; i++) {
+   
+        if(direction === -1){
+            console.log('..');
+           if ( + array[i+1] -array[i]  <= 0 ) continue
+           else return false
+        }else{
+            if ( + array[i+1] -array[i]  > 0 ) continue
+           else return false
+        }
+    }
+  
+    return true
+  }
+  
+  console.log("monotonicArrayCheck2:: ", monotonicArrayCheck2(array));
