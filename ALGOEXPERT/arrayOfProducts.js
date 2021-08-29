7114c23a2f2e5b9d171868b2f64cc0562dfe2d75
Deepak Garg <erdkgarg08@gmail.com>
2021-08-29 20:23:16 +0200

    array of products method 3 without divide

--- ALGOEXPERT/arrayOfProducts.js
+++ ALGOEXPERT/arrayOfProducts.js
@@ -21,25 +21,56 @@ function arrayOfProducts(array) {
 console.log("arrayOfProducts:: ", arrayOfProducts(array));
 
 function arrayOfProducts2(array) {
-    let result = [];
-    return  logic2(array, result);
-  }
-  
-  function logic2(array, result) {
-    let product = 1;
-    let i = 0
-    
-    // for (let i = 0; i < array.length; i++) {
-      for (let j = 0; j < array.length; j++) {
-          product*= array[j]
-      }
-      for (let j = 0; j < array.length; j++) {
-          let calc = product/array[j]
-         result.push(calc)
-    }
-    return result
-  }
-  
-  console.log("arrayOfProducts2:: ", arrayOfProducts2(array));
-  
+  let result = [];
+  return logic2(array, result);
+}
 
+function logic2(array, result) {
+  let product = 1;
+
+  // for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    product *= array[j];
+  }
+  for (let j = 0; j < array.length; j++) {
+    let calc = product / array[j];
+    result.push(calc);
+  }
+  return result;
+}
+
+console.log("arrayOfProducts2:: ", arrayOfProducts2(array));
+
+//method 3 O(n) run time and space
+
+function arrayOfProducts3(array) {
+  let result = [];
+  return logic3(array, result);
+}
+
+function logic3(array, result) {
+  let leftRunningProducts = 1;
+  let rightRunningProducts = 1;
+  let leftProduct = [];
+  let rightProduct = [];
+
+  //left
+  for (let j = 1; j <= array.length; j++) {
+    leftProduct.push(leftRunningProducts);
+    leftRunningProducts *= array[j - 1];
+  }
+  //rogjt
+
+  for (let j = array.length - 1; j >= 0; j--) {
+    rightProduct[j] = rightRunningProducts;
+    rightRunningProducts *= array[j];
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    result.push(leftProduct[i] * rightProduct[i]);
+  }
+
+  return result;
+}
+
+console.log("arrayOfProducts3:: ", arrayOfProducts3(array));
